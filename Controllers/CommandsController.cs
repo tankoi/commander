0360534722452b2e09f76318b05711d7916d62ce
Istvan Tanko <tistvan1989@gmail.com>
2022-04-21 21:58:31 +0300

    - tutorial part 1 - 1:27:58

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,37 @@
+using Commander.Data;
+using Commander.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Commander.Controllers
+{
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommanderRepository _repository;
+
+        public CommandsController(ICommanderRepository repository)
+        {
+            _repository = repository;
+        }
+
+        // GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppCommands();
+
+            return Ok(commandItems);
+        }
+
+        // GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+
+            return Ok(commandItem);
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,14 +1,11 @@
+using Commander.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Commander
 {
@@ -24,7 +21,15 @@ namespace Commander
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<CommanderContext>(options =>
+            {
+                var connectionString = Configuration.GetConnectionString("CommanderConnection");
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            });
+
             services.AddControllers();
+
+            services.AddScoped<ICommanderRepository, MockCommanderRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
