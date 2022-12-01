0f5b493461c0bdb095ce24e0d52971cd8ce3ed98
tankoi <tistvan1989@gmail.com>
2022-12-01 23:02:36 +0200

    added file controller

---
+++ Controllers/FilesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Commander.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        // POST api/files/upload
+        [HttpPost("upload")]
+        public ActionResult UploadFile(IFormFile file)
+        {
+            return Ok(new { testc = "jeee"});
+        }
+    }
+}
