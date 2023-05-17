ab53a0750493a702e716e4cfcdf99d7332f85e6d
tankoi <tistvan1989@gmail.com>
2023-05-17 20:02:12 +0300

    finished file upload

--- Controllers/FilesController.cs
+++ Controllers/FilesController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
 
 namespace Commander.Controllers
 {
@@ -7,10 +9,32 @@ namespace Commander.Controllers
     [ApiController]
     public class FilesController : ControllerBase
     {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public FilesController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+        
+
         // POST api/files/upload
         [HttpPost("upload")]
         public ActionResult UploadFile(IFormFile file)
         {
+            var wwwPath = _webHostEnvironment.WebRootPath;
+            var path = Path.Combine(wwwPath, "Uploads");
+            var fileName = Path.GetFileName(file.FileName);
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            using (FileStream stream = new FileStream(Path.Combine(path, fileName), FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+
             return Ok(new { testc = "jeee"});
         }
     }
